73e30ab2c159ff9f239fd7be4ae7cb5aee1f47dd
panzerhacker <32525528+panzerhacker@users.noreply.github.com>
2021-06-13 23:04:24 +0100

    Basic? Reverse Polish Notation Converter

---
+++ Program.fs
@@ -0,0 +1,28 @@
+﻿module RevPolNotCalc
+
+open System
+
+// Define a function to construct a message to print
+let evalRpnExpr (s : string) =
+    let solve items current =
+        match (current, items) with
+        | "+", y::x::t -> (x + y)::t
+        | "-", y::x::t -> (x - y)::t
+        | "*", y::x::t -> (x * y)::t
+        | "/", y::x::t -> (x / y)::t
+        | _ -> (float current)::items
+    (s.Split(' ') |> Seq.fold solve []).Head
+
+[<EntryPoint>]
+let main argv =
+    [ "4 2 5 * + 1 3 2 * + /"
+      "5 4 6 + /"
+      "10 4 3 + 2 * -"
+      "2 3 +"
+      "90 34 12 33 55 66 + * - + -"
+      "90 3 -"
+      "4 2 5 * +" ]
+    |> List.map (fun expr -> expr, evalRpnExpr expr)
+    |> List.iter (fun (expr, result) -> printfn "(%s) = %A" expr result)
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
